3465338ec18c427aa2bc24f945232ec5aa6998ec
bkubick <bjk2158@columbia.edu>
2023-10-27 17:05:58 -0500

    Update module description

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 /**
  * @fileoverview This file is the entry point of the application. It exports all
- * of the models, utils, etc for the AI module.
+ * of the models, utils, etc for the AI module. The module consists of
+ * model-like classes, utils, and types that are either custom or wrappers
+ * around existing models through the use of APIs.
+ * 
+ * Some of the existing models that are wrapped include:
+ * - ChatGPT
  */
 
 import * as models from './models';
